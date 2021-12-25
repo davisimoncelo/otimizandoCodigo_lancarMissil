bc9f283f537f52533ae3dbfbad378bfc9bb098eb
davisimoncelo <82762226+davisimoncelo@users.noreply.github.com>
2021-12-25 09:59:49 -0300

    Add files via upload

---
+++ Otimizando codigo - Lançar Missil.js
@@ -0,0 +1,24 @@
+var pais = 'russia';
+
+var lancarMissilNuclears = function(pais) {
+    console.log("lançando missil em 3..2..1");
+} 
+
+if (pais == 'russia') {
+    lancarMissilNuclear = function() {
+        console.log("Usar o telefone vermelho");
+    }
+
+}
+
+lancarMissilNuclear();
+
+
+//example
+
+function ola(quem) {
+    console.log("Olá " + quem);
+}
+ola("Fulaninho");
+console.log("Tchau");
+
